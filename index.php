abb57a32a2e2d1de7495d280c6336bc4f8f8330a
Slamet faisal <slametfaisal1@gmail.com>
2019-12-26 14:58:49 +0700

    Ubah Tampilan Login

--- index.php
+++ index.php
@@ -12,21 +12,25 @@ error_reporting(0);
 <head>
 	<title>tubes_pw</title>
 	<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="monster-html/css/style.css" rel="stylesheet">
-	<link href="monster-html/css/colors/blue.css" id="theme" rel="stylesheet">
 </head>
+<style>
+	html,
+	body {
+		background: #31b0d5;
+	}
+</style>
 
 <body>
-	<div class="card mx-auto mt-5" style="width: 300px;">
-		<div class="card-header" style="background:blue;">
-			<h2>Login</h2>
+	<div class="card mx-auto mt-5 border-0 shadow-lg" style="width: 400px;">
+		<div class="card-header text-center" style="background:#45d0fa;">
+			<h2 style="color: white;">LOGIN</h2>
 		</div>
 		<?php if ($_GET['pesan'] == 'gagal') { ?>
 			<h4 class="p-2 bg-danger" style="color: white;">Data yang anda masukkan belum terdaftar!</h4>
 		<?php } else if ($_GET['pesan'] == 'captcha') { ?>
 			<h4 class="p-2 bg-danger" style="color: white;">Maaf captcha yang anda masukkan salah!</h4>
 		<?php } ?>
-		<div class="body p-3">
+		<div class="body p-5">
 			<form method="post" action="cek_login.php">
 				<div class="form-group">
 					<label for="nip">NIP</label>
@@ -46,13 +50,10 @@ error_reporting(0);
 						</div>
 					</div>
 				</div>
-				<input type="submit" class="btn btn-primary" value="Login">
+				<input type="submit" class="btn btn-block" style="background: #45d0fa;color:white;" value="Masuk">
 			</form>
 		</div>
 	</div>
-	<?php
-	echo $_SESSION['captcha_code'];
-	?>
 </body>
 
 </html>
